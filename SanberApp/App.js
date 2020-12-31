624083462e11d1971dc2934db0aaa744f32761ce
Hilman Giri <hilmangp@gmail.com>
2020-12-31 22:04:04 +0700

    Added Quiz3

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -5,11 +5,12 @@ import Routes from './Tugas/Tugas13/Routes';
 import Tugas from './Tugas/Tugas14/App'
 import Tugas15 from './Tugas/Tugas15/index'
 import TugasNavigation from './Tugas/TugasNavigation';
+import Quiz3 from './Quiz3';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      < TugasNavigation />
+      < Quiz3 />
     </View>
   );
 }
